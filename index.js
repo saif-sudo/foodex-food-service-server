daf8dbc42d197bc1b50f2c340f7a94f7a0efb95d
saif-sudo <mdalisaif4@gmail.com>
2022-11-10 04:31:47 +0600

    get added

--- index.js
+++ index.js
@@ -4,27 +4,61 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+// dbUser2
+// vrD19mSrULAKMN3E
 
-// c0Xn7Hpard3P83Q3
+
+
+/*const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.agaoogq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const serviceCollection = client.db('dbUser1').collection('localhost:5000/services');
+         app.get('/services', async (req,res) =>{
+                const query = {}
+                const cursor = serviceCollection.find(query);
+                const services = await cursor.toArray();
+                res.send(services);
+
+               
+        })
+    }finally{
+        
+    }
+}
+
+run().catch(err => console.error(err));*/
 
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.agaoogq.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = `mongodb+srv://dbUser2:vrD19mSrULAKMN3E@cluster0.lzohisf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        const serviceCollection = client.db('simpleNode').collection('users');
+
+        app.get('/services', async (req,res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+           
+    })
+       /* const user = {name: 'Mehedi Ali' , email:'mehedi@gmail.com'};
+        const result = await userCollection.insertOne(user);
+        console.log(result);*/
+    }finally{
+
+    }
+}
+run().catch(err => console.log(err));
 
 app.get('/' , (req,res) => {
-    res.send('foodex-cloud-kitchen is running')
+    res.send('foodex-cloud-kitchen is running');
 
     
 })
@@ -32,3 +66,27 @@ app.get('/' , (req,res) => {
 app.listen(port , () =>{
     console.log(`foodex cloud kitchen running on ${port}`)
 } )
+
+
+
+/*
+
+
+
+
+
+const uri = `mongodb+srv://dbUser2:vrD19mSrULAKMN3E@cluster0.lzohisf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const userCollection = client.db('simpleNode').collection('users');
+        const user = {name: 'Saif Ali' , email:'saif@gmail.com'};
+        const result = await userCollection.insertOne(user);
+        console.log(result);
+    }finally{
+
+    }
+}
+run().catch(err => console.log(err));*/
+
+
