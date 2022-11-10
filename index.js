93391b1d0dd3520a2508b2634b27dec8ede251b0
saif-sudo <mdalisaif4@gmail.com>
2022-11-10 09:50:23 +0600

    new issue solve

--- index.js
+++ index.js
@@ -4,31 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-// dbUser2
-// vrD19mSrULAKMN3E
 
-
-
-/*const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.agaoogq.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
-        const serviceCollection = client.db('dbUser1').collection('localhost:5000/services');
-         app.get('/services', async (req,res) =>{
-                const query = {}
-                const cursor = serviceCollection.find(query);
-                const services = await cursor.toArray();
-                res.send(services);
-
-               
-        })
-    }finally{
-        
-    }
-}
-
-run().catch(err => console.error(err));*/
 
 app.use(cors());
 app.use(express.json());
@@ -88,24 +64,6 @@ app.listen(port , () =>{
 
 
 
-/*
 
-
-
-
-
-const uri = `mongodb+srv://dbUser2:vrD19mSrULAKMN3E@cluster0.lzohisf.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
-        const userCollection = client.db('simpleNode').collection('users');
-        const user = {name: 'Saif Ali' , email:'saif@gmail.com'};
-        const result = await userCollection.insertOne(user);
-        console.log(result);
-    }finally{
-
-    }
-}
-run().catch(err => console.log(err));*/
 
 
